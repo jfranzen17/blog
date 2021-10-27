aaa36a4363f2f119553ea8a40991a98812ed14be
Johan <jfranzen17@gmail.com>
2021-10-28 00:15:26 +0200

    added dummy content to homepage

--- frontend/website/.next/static/chunks/webpack.js
+++ frontend/website/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "127cb2df48784dbb"; }
+/******/ 		__webpack_require__.h = function() { return "f061e4cdab43c7fa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/website/pages/_app.js
+++ frontend/website/pages/_app.js
@@ -1,6 +1,9 @@
 import "../styles/layout.css"
+
 import "../styles/components/header.css"
 import "../styles/components/footer.css"
+
+import "../styles/pages/homepage.css"
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps}/>
